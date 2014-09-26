e9355ed0cc8eac7df766557d442ff7e49ebbbd26
jhlee1025 <jhlee1025@hotmail.com>
2014-09-26 18:44:18 -0400

    updated

--- www/js/starPrinter.js
+++ www/js/starPrinter.js
@@ -1,10 +1,10 @@
 var starPrinter = {
-    createEvent: function(title, orderId, successCallback, errorCallback) {
+    print: function(orderId, successCallback, errorCallback) {
         cordova.exec(
             successCallback, // success callback function
             errorCallback, // error callback function
             'StarPrinter', // mapped to our native Java class called "StarPrinter"
-            'addCalendarEntry', // with this action name
+            'print', // with this action name
             [{                  // and this array of custom arguments to create our entry
                 "orderId": orderId
             }]
