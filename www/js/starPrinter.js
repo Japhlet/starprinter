5de6f678b1fa62d857c34789c3392dde2fc3e8e6
jhlee1025 <jhlee1025@hotmail.com>
2014-09-29 17:45:57 -0400

    added

--- www/js/starPrinter.js
+++ www/js/starPrinter.js
@@ -9,6 +9,33 @@ var starPrinter = {
                 "orderId": orderId
             }]
         ); 
+     },
+    openCashDrawer: function(successCallback, errorCallback) {
+        cordova.exec(
+            successCallback, // success callback function
+            errorCallback, // error callback function
+            'StarPrinter', // mapped to our native Java class called "StarPrinter"
+            'openCashDrawer', // with this action name
+            [{"":""}]
+        ); 
+     },
+    searchTCPPrinters: function(successCallback, errorCallback) {
+        cordova.exec(
+            successCallback, // success callback function
+            errorCallback, // error callback function
+            'StarPrinter', // mapped to our native Java class called "StarPrinter"
+            'searchTCPPrinters', // with this action name
+            [{"":""}]
+        ); 
+     },
+    searchBTPrinters: function(successCallback, errorCallback) {
+        cordova.exec(
+            successCallback, // success callback function
+            errorCallback, // error callback function
+            'StarPrinter', // mapped to our native Java class called "StarPrinter"
+            'searchBTPrinters', // with this action name
+            [{"":""}]
+        ); 
      }
 }
 module.exports = starPrinter;
