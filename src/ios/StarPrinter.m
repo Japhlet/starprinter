0a3f926b2f268baa8cae81fb6b364e0b87806ad9
jhlee1025 <jhlee1025@hotmail.com>
2014-10-03 18:25:42 -0400

    added

---
+++ src/ios/StarPrinter.m
@@ -0,0 +1,129 @@
+//
+//  StarPrinter.m
+//  PosAndroid
+//
+//  Created by Jay Lee on 9/30/14.
+//
+//
+
+#import "StarPrinter.h"
+#import <StarIO/SMPort.h>
+#import <StarIO/SMBluetoothManager.h>
+#import "RasterDocument.h"
+#import "StarBitmap.h"
+#import <sys/time.h>
+#include <unistd.h>
+
+@implementation StarPrinter
++ (void)sendCommand:(NSData *)commandsToPrint portName:(NSString *)portName portSettings:(NSString *)portSettings timeoutMillis:(u_int32_t)timeoutMillis
+{
+    int commandSize = (int)[commandsToPrint length];
+    unsigned char *dataToSentToPrinter = (unsigned char *)malloc(commandSize);
+    [commandsToPrint getBytes:dataToSentToPrinter];
+    
+    SMPort *starPort = nil;
+    @try
+    {
+        starPort = [SMPort getPort:portName :portSettings :timeoutMillis];
+        if (starPort == nil)
+        {
+            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Fail to Open Port"
+                                                            message:@""
+                                                           delegate:nil
+                                                  cancelButtonTitle:@"OK"
+                                                  otherButtonTitles:nil];
+            [alert show];
+            return;
+        }
+        
+        StarPrinterStatus_2 status;
+        [starPort beginCheckedBlock:&status :2];
+        if (status.offline == SM_TRUE) {
+            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error"
+                                                            message:@"Printer is offline"
+                                                           delegate:nil
+                                                  cancelButtonTitle:@"OK"
+                                                  otherButtonTitles:nil];
+            [alert show];
+            return;
+        }
+        
+        struct timeval endTime;
+        gettimeofday(&endTime, NULL);
+        endTime.tv_sec += 30;
+        
+        int totalAmountWritten = 0;
+        while (totalAmountWritten < commandSize)
+        {
+            int remaining = commandSize - totalAmountWritten;
+            int amountWritten = [starPort writePort:dataToSentToPrinter :totalAmountWritten :remaining];
+            totalAmountWritten += amountWritten;
+            
+            struct timeval now;
+            gettimeofday(&now, NULL);
+            if (now.tv_sec > endTime.tv_sec)
+            {
+                break;
+            }
+        }
+        
+        if (totalAmountWritten < commandSize)
+        {
+            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Printer Error"
+                                                            message:@"Write port timed out"
+                                                           delegate:nil
+                                                  cancelButtonTitle:@"OK"
+                                                  otherButtonTitles:nil];
+            [alert show];
+        }
+        
+        starPort.endCheckedBlockTimeoutMillis = 30000;
+        [starPort endCheckedBlock:&status :2];
+        if (status.offline == SM_TRUE) {
+            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error"
+                                                            message:@"Printer is offline"
+                                                           delegate:nil
+                                                  cancelButtonTitle:@"OK"
+                                                  otherButtonTitles:nil];
+            [alert show];
+            return;
+        }
+    }
+    @catch (PortException *exception)
+    {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Printer Error"
+                                                        message:@"Write port timed out"
+                                                       delegate:nil
+                                              cancelButtonTitle:@"OK"
+                                              otherButtonTitles:nil];
+        [alert show];
+    }
+    @finally
+    {
+        free(dataToSentToPrinter);
+        [SMPort releasePort:starPort];
+    }
+}
+
++ (void)openCashDrawerWithPortname: (CDVInvokedUrlCommand*)command
+{
+    id level = [command.arguments objectAtIndex:0];
+    id message = [command.arguments objectAtIndex:1];
+
+    NSString *portName = @"TCP:10.1.1.107";
+    NSString *portSettings = @"";
+    int drawerNumber = 1;
+    
+    unsigned char opencashdrawer_command = 0x00;
+    
+    if (drawerNumber == 1) {
+        opencashdrawer_command = 0x07; //BEL
+    }
+    else if (drawerNumber == 2) {
+        opencashdrawer_command = 0x1a; //SUB
+    }
+    
+    NSData *commands = [NSData dataWithBytes:&opencashdrawer_command length:1];
+    [self sendCommand:commands portName:portName portSettings:portSettings timeoutMillis:10000];
+}
+@end
