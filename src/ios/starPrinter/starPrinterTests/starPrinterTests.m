9456b47786644d18f27394c3666f59e55a32744e
jhlee1025 <jhlee1025@hotmail.com>
2014-10-03 18:45:04 -0400

    Initial Commit

---
+++ src/ios/starPrinter/starPrinterTests/starPrinterTests.m
@@ -0,0 +1,40 @@
+//
+//  starPrinterTests.m
+//  starPrinterTests
+//
+//  Created by Jay Lee on 10/3/14.
+//  Copyright (c) 2014 Jay Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface starPrinterTests : XCTestCase
+
+@end
+
+@implementation starPrinterTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
