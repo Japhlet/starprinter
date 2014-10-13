2d5c29cdb133651bcaea4abed2dbd06a489e9701
jhlee1025 <jhlee1025@hotmail.com>
2014-10-13 10:45:21 -0400

    updated

---
+++ src/ios/starPrinter/StarPrinter.h
@@ -0,0 +1,20 @@
+//
+//  StarPrinter.h
+//  PosAndroid
+//
+//  Created by Jay Lee on 9/30/14.
+//
+//
+
+#import <Cordova/CDV.h>
+
+@interface StarPrinter : CDVPlugin {}
+
++ (void)openCashDrawer:(CDVInvokedUrlCommand*)command;
+
++ (void)sendCommand:(NSData *)commandsToPrint
+                    portName:(NSString *)portName
+                    portSettings:(NSString *)portSettings
+                    timeoutMillis:(u_int32_t)timeoutMillis;
+
+@end
